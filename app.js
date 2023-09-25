20608e5f07756e5359f5cd00faf5ce8cf05d3492
Duncan <duncanstewart96@gmail.com>
2023-09-25 11:52:49 -0400

    split routing and logic into separate functionality

--- app.js
+++ app.js
@@ -14,7 +14,113 @@ server.use(bodyParser.json());
 let localStorage = [];
 
 
-
+function patchTicketsLogic(req){
+  const id = req.query.ticketID;
+  const stat = req.body.status;
+  let response = new Promise((resolve,reject) => {
+    if (req.body.payload.role === 'admin'){
+      dao.isTicketPending(id)
+      .then((data) => {
+        if(data.Items[0].status === 'pending'){
+          dao.updateTicketByID(id,stat,req.body.payload.username)
+            .then((data) =>{
+              resolve({"message":`ticket ${id} has been set to ${stat}`,"status":200});
+            })
+            .catch((err) => {
+              reject({"message":err,"status":500});
+            })
+        }
+        else{
+          resolve({"message":"cannot change a resolved ticket","status":403});
+        }
+      })
+      .catch((err) => {
+        reject({"message":err,"status":400});
+      })
+    }
+    else{
+      resolve({"message":"you do not have permission to do this","status":403});
+    }
+  })
+  return(response);
+}
+function getTicketsLogic(req){
+  let response = new Promise((resolve,reject) => {
+  const body = req.body;
+  if(body.payload){
+    if(body.payload.role === 'admin'){
+      if(req.query.status){
+        dao.retrieveTicketsByStatus(req.query.status)
+        .then((data) => {
+          localStorage = data.Items;
+          resolve({"message":localStorage,"status":200});
+        })
+        .catch((err) => {
+          reject({"message":err, "status":400});
+        })
+      }
+      else{
+          dao.retrieveAllTickets()
+            .then((data) => {
+              localStorage = data.Items;
+              resolve({"message":localStorage,"status":200});
+            })
+            .catch((err) => {
+              reject({"message":err, "status":500});
+            })
+      }
+    }
+    else if(body.payload.role === 'employee'){
+      if(req.query.type){
+        dao.retrieveTicketsByUserAndType(body.payload.username,req.query.type)
+        .then((data) => {
+          resolve({"message":data.Items,"status":200});
+        })
+        .catch((err) => {
+          reject({"message":err, "status":500});
+        })
+      }
+      else{
+        dao.retrieveTicketsByUser(body.payload.username)
+        .then((data) => {
+          resolve({"message":data.Items,"status":200});
+        })
+        .catch((err) => {
+          reject({"message":err, "status":500});
+        })
+      }
+    }
+  }
+  else{
+    reject({"message":"you must log in to do this", "status":401});
+  }
+  })
+  return(response);
+}
+function postTicketsLogic(req){
+  let response = new Promise((resolve,reject) => {
+    const body = req.body;
+    if(req.body.payload){
+      if(req.body.valid){
+        if(req.body.type){
+          dao.createTicket(uuid.v4(),body.amount,body.description, body.payload.username, type=body.type);
+        }
+        else{
+          dao.createTicket(uuid.v4(),body.amount,body.description, body.payload.username);
+        }
+        
+        resolve({"message":"Ticket submitted!","status": 200});
+        }
+      else{
+        reject({"message":body.reason,"status":400})
+      }
+    }
+    else{
+      reject({"message":"you must be logged in to post a ticket","status":401});
+    }
+  })
+  return response;
+}
 // Validation functions
 function isUsernameValid(username){
   let res = new Promise((resolve,reject) => {
@@ -34,28 +140,47 @@ function isUsernameValid(username){
   return(res); 
 }
 function validateNewAccount(req, res, next){
-  if(!req.body.username.trim() || !req.body.password.trim()){
+  if(!req.body.username || !req.body.password){
       req.body.valid = false;
       next();
-  }else{
-      req.body.valid = true;
+  }
+  else{
+    if(!req.body.username.trim() || !req.body.password.trim()){
+      req.body.valid = false;
       next();
+    }
+    else{
+        req.body.valid = true;
+        next();
+    } 
   }
 }
 function validateNewTicket(req, res, next){
-  if(!req.body.amount.trim()){
+  if(!req.body.amount){
       req.body.valid = false;
       req.body.reason = "you must include an amount"
       next();
   }
-  if(!req.body.description.trim()){
+  if(!req.body.description){
     req.body.valid = false;
     req.body.reason = "you must include a description"
     next();
-}
+  }
   else{
-      req.body.valid = true;
+    if(!req.body.amount.trim()){
+      req.body.valid = false;
+      req.body.reason = "you must include an amount"
       next();
+    }
+    if(!req.body.description.trim()){
+      req.body.valid = false;
+      req.body.reason = "you must include a description"
+      next();
+    }
+    else{
+      req.body.valid = true;
+      next()
+    }
   }
 }
 function validatePermission(req, res, next){
@@ -72,6 +197,7 @@ function validatePermission(req, res, next){
     })
   }
   else{
+    req.body.loggedin = false;
     next();
   } 
 }
@@ -83,163 +209,146 @@ function validatePermission(req, res, next){
 
 // Ticket endpoints 
 server.patch('/tickets',validatePermission, (req,res) => {
-  const id = req.query.ticketID;
-  const stat = req.body.status;
-  if (req.body.payload.role === 'admin'){
-    dao.isTicketPending(id)
-    .then((data) => {
-      if(data.Items[0].status === 'pending'){
-        dao.updateTicketByID(id,stat,req.body.payload.username)
-          .then((data) =>{
-            res.send(`ticket ${id} has been set to ${stat}`);
-          })
-          .catch((err) => {
-            res.send(err);
-          })
-      }
-      else{
-        res.send("cannot change a resolved ticket")
-      }
-    })
-    .catch((err) => {
-      res.send(err);
-    })
-  }
-  else{
-    res.send("you do not have permission to do this")
-  }
+  patchTicketsLogic(req)
+  .then((data) => {
+    res.status(data.status);
+    res.send(data.message);
+  })
+  .catch((err) =>{
+    res.status(err.status);
+    res.send("there was an error \n"+err.message);
+  })
 })
 
 server.get('/tickets',validatePermission, (req,res) =>{
-  const body = req.body;
-  if(body.payload){
-    if(body.payload.role === 'admin'){
-      if(req.query.status){
-        dao.retrieveTicketsByStatus(req.query.status)
-        .then((data) => {
-          localStorage = data.Items;
-          res.send(localStorage);
-        })
-        .catch((err) => {
-          res.send(err);
-        })
-      }
-      else{
-          dao.retrieveAllTickets()
-            .then((data) => {
-              localStorage = data.Items;
-              res.send(localStorage);
-            })
-            .catch((err) => {
-              res.send(err);
-            })
-      }
-    }
-    else if(body.payload.role === 'employee'){
-      if(req.query.type){
-        dao.retrieveTicketsByUserAndType(body.payload.username,req.query.type)
-        .then((data) => {
-          res.send(data.Items);
-        })
-        .catch((err) => {
-          res.send(err);
-        })
-      }
-      else{
-        dao.retrieveTicketsByUser(body.payload.username)
-        .then((data) => {
-         res.send(data.Items);
-        })
-        .catch((err) => {
-         res.send(err);
-        })
-      }
-    }
-  }
-  else{
-    res.send("you must log in to do this")
-  }
+  getTicketsLogic(req)
+  .then((data) => {
+    res.status(data.status);
+    res.send(data.message);
+  })
+  .catch((err) => {
+    res.status(err.status);
+    res.send(err.message);
+  })
 })
 
 server.post('/tickets',validateNewTicket,validatePermission, (req, res) => {
-  const body = req.body;
-  if(req.body.payload){
-    if(req.body.valid){
-      if(req.body.type){
-        dao.createTicket(uuid.v4(),body.amount,body.description, body.payload.username, type=body.type);
-      }
-      else{
-        dao.createTicket(uuid.v4(),body.amount,body.description, body.payload.username);
-      }
-      
-      res.send("Ticket submitted!");
-      }
-    else{
-      res.send(body.reason);
-    }
-  }
-  else{
-    res.send("you must be logged in to post a ticket");
-  }
+  postTicketsLogic(req)
+  .then((data) => {
+    res.status(data.status);
+    res.send(data.message);
+  })
+  .catch((err) => {
+    res.status(err.status);
+    res.send(err.message);
+  })
 })
 
 
+function setRoleLogic(req){
+  let response = new Promise((resolve,reject) => {
+    if(req.body.payload){
+      if(req.body.payload.role === 'admin'){
+        dao.updateUserByUsername(req.query.username,req.query.role)
+        .then((data) => {
+          resolve({"message":"role updated!","status":200});
+        })
+        .catch((err) => {
+          reject({"message":err,"status":500});
+        })
+      }
+      else{
+          reject({"message":"you do not have permission to do this","status":403});
+        }
+    }
+    else{
+      reject({"message":"you must log in","status":402});
+    }
+    
+    
+  })
+  return response;
+}
+function createAccountLogic(req){
+  let response = new Promise((resolve,reject) => {
+  const body = req.body;
+  if(req.body.valid){
+    isUsernameValid(body.username)
+    .then((message) => {
+      dao.registerNewUser(body.username,body.password);
+      resolve({"message":"Account created!", "status":200});
+    })
+    .catch((message) => {
+      reject({"message":message, "status":400});
+    })
+  }
+  else{
+    reject({"message":"you are missing required inputs\n Please include:\n1) a username\n2) a password",
+    "status":400});
+  }
+  })
+  return response;
+}
+function loginLogic(req){
+  let response = new Promise((resolve,reject) => {
+  const body = req.body;
+  dao.loginToAccount(body.username,body.password)
+  .then((data) => {
+    if(data.Count){
+      const token = jwtUtil.createJWT(body.username, data.Items[0].role);
+      resolve({"message":"login successful!", "token": token, "status":200, 
+      "user": body.username})
+    }
+    else{
+      reject({"message":"Invalid credentials", "status":402});
+    }
+  })
+  .catch((err) => {
+    reject({"message":"There was an error\n"+err, "status":500});
+  })
+  })
+  return response;
+}
 
 
 
 
 // Users endpoints
 server.patch('/users/setrole',validatePermission, (req,res) => {
-  if(req.body.payload.role === 'admin'){
-    dao.updateUserByUsername(req.query.username,req.query.role)
-    .then((data) => {
-      res.send("role updated!");
-    })
-    .catch((err) => {
-      res.send(err);
-    })
-  }
-  else{
-    res.send("you do not have permission to do this")
-  }
+  setRoleLogic(req)
+  .then((data) => {
+    res.status(data.status);
+    res.send(data.message);
+  })
+  .catch((err) =>{
+    res.status(err.status);
+    res.send("there was an error \n"+err.message);
+  })
 })
 server.post('/users',validateNewAccount, (req, res) => {
-  const body = req.body;
-  if(req.body.valid){
-    isUsernameValid(body.username)
-    .then((message) => {
-      dao.registerNewUser(body.username,body.password);
-      res.send(message+"\n\nAccount created!");
-    })
-    .catch((message) => {
-      res.send(message);
-    })
-  }
-  else{
-    res.send("you are missing required inputs\n Please include:\n1) a username\n2) a password\n3) whether you are an administrator")
-  }
+  createAccountLogic(req)
+  .then((data) => {
+    res.status(data.status);
+    res.send(data.message);
+  })
+  .catch((err) =>{
+    res.status(err.status);
+    res.send("there was an error \n"+err.message);
+  })
 })
 
 server.post('/users/login', (req,res) => {
-  const body = req.body;
-  dao.loginToAccount(body.username,body.password)
+  loginLogic(req)
   .then((data) => {
-    if(data.Count){
-      const token = jwtUtil.createJWT(body.username, data.Items[0].role);
-      res.setHeader("Content-Type", "application/json") ;
-      res.setHeader("User-logged-in", body.username);
-      res.send({
-        message: "login successful",
-        token : token
-      }).status(200);
-    }
-    else{
-      res.send("Invalid credentials");
-    }
+    console.log(data);
+    res.status(data.status);
+    res.send(data.message +"\n you are logged in as: "+ data.user +"\nHere is your auth token: "+ data.token);
   })
-  .catch((err) => {
-    res.send("invalid credentials")
+  .catch((err) =>{
+    res.status(err.status);
+    res.send("there was an error \n"+err.message);
   })
+  
 })
 
 
